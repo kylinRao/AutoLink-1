67b7b3ee347b15f0f160da47a0bf28e056def8d0
kylinRao <975168367@qq.com>
2020-03-31 15:04:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-nohup python AutoLink.py runserver -h 10.0.54.5 &
+NAME='AutoLink'
+echo $NAME
+PID=`ps -ef | grep "$NAME" | grep python | grep -v grep | awk '{print $2}'`
+echo "当前进程号为：$PID"
+echo "---------------"
+for pid in $PID
+do
+sleep 2
+kill -9  $pid
+echo "killed [$pid]"
+done
+nohup python AutoLink.py runserver -h 0.0.0.0 &
